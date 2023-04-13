6b4d97041769aec2b5b7b55efed520a03a3bf9e6
yuuchien.yang@gmail.com <yuuchien.yang@gmail.com>
2023-04-13 17:34:17 +0800

    featured ai response

--- src/pages/GraphView/GraphView.tsx
+++ src/pages/GraphView/GraphView.tsx
@@ -1,6 +1,9 @@
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components/macro';
 import NetworkGraph from '../../components/NetworkGraph';
 import { openAI } from '../../utils/openAI';
+import { useAppSelector } from '../../app/hooks';
+import { selectProfile } from '../../app/loginSlice';
 
 const GraphWrapper = styled.div`
   width: 1280px;
@@ -9,12 +12,52 @@ const GraphWrapper = styled.div`
 `;
 
 const GraphView = () => {
+  const profile = useAppSelector(selectProfile);
+  const [titles, setTitles] = useState<string[]>([]);
+  const [recentNotes, setRecentNotes] = useState<string[]>([]);
+  const [isAiTyping, setIsAiTyping] = useState(false);
+  const [summeries, setSummeries] = useState('');
+
+  useEffect(() => {
+    if (!profile.isLogin) {
+      return;
+    }
+
+    const notesArr = [...profile.notes]
+      .filter((note) => note.category !== 'shared')
+      .sort((a, b) => b.create_time - a.create_time)
+      .reduce((notes: string[], note) => [...notes, note.content], [])
+      .slice(0, 3);
+    const titlesArr = [...profile.notes].reduce(
+      (titles: string[], note) => [...titles, note.title],
+      []
+    );
+
+    setRecentNotes(notesArr);
+    setTitles(titlesArr);
+  }, [profile.isLogin]);
+
+  const handleAiResponse = async () => {
+    setIsAiTyping(true);
+    const res = await openAI.summerize(titles, recentNotes);
+    setIsAiTyping(false);
+    setSummeries(res);
+  };
+
   return (
     <>
-      <button onClick={() => openAI.summerize()}>ai測試</button>
-      <GraphWrapper>
-        <NetworkGraph />
-      </GraphWrapper>
+      <div>
+        <GraphWrapper>
+          <NetworkGraph />
+        </GraphWrapper>
+        {isAiTyping ? (
+          ''
+        ) : (
+          <button onClick={handleAiResponse}>ai信仰回顧</button>
+        )}
+        {isAiTyping ? <div>ai回答中...</div> : ''}
+        <p>{summeries}</p>
+      </div>
     </>
   );
 };
--- src/utils/openAI.js
+++ src/utils/openAI.js
@@ -6,13 +6,18 @@ const configuration = new Configuration({
 });
 
 export const openAI = {
-  async summerize() {
+  async summerize(titles, recentNotes) {
     const openai = new OpenAIApi(configuration);
-    const prompt = 'Hello!!';
+    const prompt = `請以第一人稱,根據我最近的筆記,以兩句話對我簡短做信仰狀況回顧,
+並根據我的筆記列表,推薦我複習的3篇筆記:
+我最近的筆記:${recentNotes},
+我的筆記列表:${titles},
+`;
+
     const res = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [{role:'user',content:prompt}],
     });
-    console.log(res.data.choices[0].message.content);
+    return res.data.choices[0].message.content;
   }
 }
