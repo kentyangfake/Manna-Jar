36924363bc74a2428f2a4cf44ef3f25a49379deb
yuuchien.yang@gmail.com <yuuchien.yang@gmail.com>
2023-05-10 17:44:51 +0800

    refactor graphView filter with map options

---
+++ src/pages/GraphView/AiAssistant.tsx
@@ -0,0 +1,82 @@
+import { useState } from 'react';
+import { useAppSelector } from '../../redux/hooks';
+import { selectFontSize, selectProfile } from '../../redux/loginSlice';
+import { openAI } from '../../utils/openAI';
+import * as styles from '../../utils/styles';
+import { parseFontSize } from '../../utils/utils';
+
+interface Props {
+  titles: string[];
+  recentNotes: string[];
+  summeries: string;
+  setSummeries: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const AiAssistant = ({
+  titles,
+  recentNotes,
+  summeries,
+  setSummeries,
+}: Props) => {
+  const profile = useAppSelector(selectProfile);
+  const fontSizeNum = useAppSelector(selectFontSize);
+  const fontSize = parseFontSize(fontSizeNum);
+  const [toggled, setToggled] = useState(false);
+  const [isAiTyping, setIsAiTyping] = useState(false);
+
+  const handleAiResponse = async () => {
+    setIsAiTyping(true);
+    const res = await openAI.summarize(titles, recentNotes);
+    setIsAiTyping(false);
+    setSummeries(res);
+  };
+
+  return (
+    <>
+      <div
+        className={`${styles.theme} fixed flex flex-col z-30 top-0 right-0 h-full w-7 border-l border-stone-500`}
+      >
+        <div className={`${styles.theme} border-b h-8 -mt-[1px]`}></div>
+        <div
+          className={`${styles.navButtonSmall} ${
+            toggled
+              ? 'bg-blue-50 border-b'
+              : 'bg-blue-100 border-b-2 hover:bg-blue-50 hover:border-b-4 hover:border-l'
+          }  h-32`}
+          onClick={() => setToggled((prev) => !prev)}
+        >
+          {toggled ? '›' : 'AI'}
+        </div>
+        <div className={`${styles.theme} grow`}></div>
+      </div>
+      <div
+        className={`${
+          toggled
+            ? 'fixed flex flex-col z-20 top-8 right-0 h-full w-96 border-x pl-3 pr-10 text-stone-500 border-stone-500 bg-[rgba(214,211,208,0.7)] backdrop-blur'
+            : 'hidden'
+        }`}
+      >
+        <div className="border-b border-stone-500 py-3 text-2xl italic tracking-widest">
+          信仰回顧
+        </div>
+        <div
+          className={`${fontSize} pt-3 tracking-wider leading-relaxed overflow-y-auto`}
+        >
+          {summeries}
+        </div>
+        {profile.notes.length > 5 && (
+          <div
+            className={`${styles.themeButtonNoBg} ${
+              isAiTyping && 'bg-stone-100'
+            } self-center mt-10 w-32 h-10 border`}
+            onClick={handleAiResponse}
+          >
+            {isAiTyping ? '回答中...' : 'AI回顧'}
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default AiAssistant;
--- src/pages/GraphView/GraphView.tsx
+++ src/pages/GraphView/GraphView.tsx
@@ -5,21 +5,17 @@ import NetworkGraph from '../../components/NetworkGraph';
 import ToolBar from '../../components/ToolBar';
 import { useAppSelector } from '../../redux/hooks';
 import { selectFontSize, selectProfile } from '../../redux/loginSlice';
-import { openAI } from '../../utils/openAI';
 import * as styles from '../../utils/styles';
-import { parseFontSize } from '../../utils/utils';
+import AiAssistant from './AiAssistant';
 
 const GraphView = () => {
   const profile = useAppSelector(selectProfile);
   const fontSizeNum = useAppSelector(selectFontSize);
-  const fontSize = parseFontSize(fontSizeNum);
   const navigate = useNavigate();
   const [titles, setTitles] = useState<string[]>([]);
   const [recentNotes, setRecentNotes] = useState<string[]>([]);
   const [filtBy, setFiltBy] = useState('all');
-  const [isAiTyping, setIsAiTyping] = useState(false);
   const [summeries, setSummeries] = useState('讓AI幫你回顧最近的筆記吧!');
-  const [toggled, setToggled] = useState(false);
 
   useEffect(() => {
     if (!profile.isLogin) {
@@ -44,12 +40,28 @@ const GraphView = () => {
     setTitles(titlesArr);
   }, [profile.isLogin]);
 
-  const handleAiResponse = async () => {
-    setIsAiTyping(true);
-    const res = await openAI.summarize(titles, recentNotes);
-    setIsAiTyping(false);
-    setSummeries(res);
-  };
+  const filterOptions = [
+    {
+      lable: '全部筆記',
+      value: 'all',
+      style: filtBy === 'all' ? 'bg-stone-100' : 'bg-stone-300',
+    },
+    {
+      lable: '聚會崇拜',
+      value: 'sermon',
+      style: filtBy === 'sermon' ? 'bg-lime-100' : 'bg-stone-300',
+    },
+    {
+      lable: '個人靈修',
+      value: 'devotion',
+      style: filtBy === 'devotion' ? 'bg-violet-100' : 'bg-stone-300',
+    },
+    {
+      lable: '分享收藏',
+      value: 'shared',
+      style: filtBy === 'shared' ? 'bg-amber-100' : 'bg-stone-300',
+    },
+  ];
 
   return (
     <div className="flex h-full w-full">
@@ -59,38 +71,14 @@ const GraphView = () => {
         </div>
         <div className="flex relative justify-center items-center grow pl-5 pr-14 py-5 sky tracking-widest">
           <div className={`absolute z-10 top-10 left-20 flex flex-col gap-2`}>
-            <div
-              className={`py-1 px-2 border rounded-full text-xs ${
-                styles.themeButtonNoBg
-              } ${filtBy === 'all' ? 'bg-stone-100' : 'bg-stone-300'}`}
-              onClick={() => setFiltBy('all')}
-            >
-              全部筆記
-            </div>
-            <div
-              className={`py-1 px-2 border rounded-full text-xs ${
-                styles.themeButtonNoBg
-              } ${filtBy === 'sermon' ? 'bg-lime-100' : 'bg-stone-300'}`}
-              onClick={() => setFiltBy('sermon')}
-            >
-              聚會崇拜
-            </div>
-            <div
-              className={`py-1 px-2 border rounded-full text-xs ${
-                styles.themeButtonNoBg
-              } ${filtBy === 'devotion' ? 'bg-violet-100' : 'bg-stone-300'}`}
-              onClick={() => setFiltBy('devotion')}
-            >
-              個人靈修
-            </div>
-            <div
-              className={`py-1 px-2 border rounded-full text-xs ${
-                styles.themeButtonNoBg
-              } ${filtBy === 'shared' ? 'bg-amber-100' : 'bg-stone-300'}`}
-              onClick={() => setFiltBy('shared')}
-            >
-              分享收藏
-            </div>
+            {filterOptions.map((filter) => (
+              <div
+                className={`${filter.style} ${styles.themeButtonNoBg} py-1 px-2 border rounded-full text-xs`}
+                onClick={() => setFiltBy(filter.value)}
+              >
+                {filter.lable}
+              </div>
+            ))}
           </div>
           <Star className={`star1 fixed z-20 top-[10%] right-[20%] h-14`} />
           <Star className={`star2 fixed z-20 bottom-[6%] right-[16%] h-24`} />
@@ -106,48 +94,12 @@ const GraphView = () => {
           </div>
         </div>
       </div>
-      <div
-        className={`${styles.theme} fixed flex flex-col z-30 top-0 right-0 h-full w-7 border-l border-stone-500`}
-      >
-        <div className={`${styles.theme} border-b h-8 -mt-[1px]`}></div>
-        <div
-          className={`${styles.navButtonSmall} ${
-            toggled
-              ? 'bg-blue-50 border-b'
-              : 'bg-blue-100 border-b-2 hover:bg-blue-50 hover:border-b-4 hover:border-l'
-          }  h-32`}
-          onClick={() => setToggled((prev) => !prev)}
-        >
-          {toggled ? '›' : 'AI'}
-        </div>
-        <div className={`${styles.theme} grow`}></div>
-      </div>
-      <div
-        className={`${
-          toggled
-            ? 'fixed flex flex-col z-20 top-8 right-0 h-full w-96 border-x pl-3 pr-10 text-stone-500 border-stone-500 bg-[rgba(214,211,208,0.7)] backdrop-blur'
-            : 'hidden'
-        }`}
-      >
-        <div className="border-b border-stone-500 py-3 text-2xl italic tracking-widest">
-          信仰回顧
-        </div>
-        <div
-          className={`${fontSize} pt-3 tracking-wider leading-relaxed overflow-y-auto`}
-        >
-          {summeries}
-        </div>
-        {profile.notes.length > 5 && (
-          <div
-            className={`${styles.themeButtonNoBg} ${
-              isAiTyping && 'bg-stone-100'
-            } self-center mt-10 w-32 h-10 border`}
-            onClick={handleAiResponse}
-          >
-            {isAiTyping ? '回答中...' : 'AI回顧'}
-          </div>
-        )}
-      </div>
+      <AiAssistant
+        titles={titles}
+        recentNotes={recentNotes}
+        summeries={summeries}
+        setSummeries={setSummeries}
+      />
     </div>
   );
 };
